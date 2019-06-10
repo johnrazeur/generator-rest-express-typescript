9ab2353180e49a02b745d46c82bead059c856d27
Clément Lafont <johnrazeur@gmail.com>
2019-06-10 21:33:25 +0200

    Add linter in pretest

--- generators/app/templates/eslintrc.js
+++ generators/app/templates/eslintrc.js
@@ -3,8 +3,11 @@ module.exports =  {
   extends:  [
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
- parserOptions:  {
+  parserOptions:  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
     sourceType:  'module',  // Allows for the use of imports
-  }
+  },
+  rules:  {
+    "@typescript-eslint/no-explicit-any": "off"
+  },
 };
